40782a70b2746bf896732e6c022ae6e8d2c8706c
Vedranafeit <petreskavedrana@gmail.com>
2024-07-26 15:01:12 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            "black": "060606"
+        },
+    },
+    plugins: [],
+}
