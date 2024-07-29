92af893a7c3545c5ca83c07a475e1e4a10746d05
Vedranafeit <petreskavedrana@gmail.com>
2024-07-29 14:12:25 +0200

    initial commit

--- database/migrations/0001_01_01_000002_create_queued_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_queued_jobs_table.php
@@ -50,8 +50,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
-        Schema::dropIfExists('job_batches');
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('queued_jobs');
+        Schema::dropIfExists('queued_job_batches');
+        Schema::dropIfExists('queued_failed_jobs');
     }
 };
